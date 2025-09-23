c5b6ec148b96d7bc125207425e0408577f3ea643
SnowyPeax <johbr@itu.dk>
2025-09-23 20:53:08 +0200

    add player scene with simple movement

---
+++ Characters/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var move_speed: int = 500
+
+func _physics_process(_delta: float) -> void:
+	var move_direction: Vector2 = _get_input_direction()
+	
+	velocity = move_direction * move_speed
+	move_and_slide()
+
+
+func _get_input_direction() -> Vector2:
+	var direction = Vector2(0.0,0.0)
+	
+	if Input.is_action_pressed("right"):
+		direction += Vector2.RIGHT
+	if Input.is_action_pressed("left"):
+		direction += Vector2.LEFT
+	if Input.is_action_pressed("up"):
+		direction += Vector2.UP
+	if Input.is_action_pressed("down"):
+		direction += Vector2.DOWN
+	
+	return direction.normalized()
