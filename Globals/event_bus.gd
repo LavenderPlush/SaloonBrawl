4ac6a3364f5cb9d743315a09242432a685c6c85d
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-09-26 23:50:44 +0200

    Add global event bus and scoring

---
+++ Globals/event_bus.gd
@@ -0,0 +1,6 @@
+# The event bus is a global script that allows multiple
+# objects to send signals through / listen to.
+extends Node
+
+@warning_ignore("unused_signal")
+signal add_money(amount: float)
