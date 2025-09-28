1d80d852c12b8c70230e7b09bdd6edbb6cb45173
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-09-28 15:43:03 +0200

    Add NPC movement

--- Characters/Patron/patron.gd
+++ Characters/Patron/patron.gd
@@ -2,10 +2,23 @@ extends CharacterBody2D
 class_name Patron
 
 signal death
+signal moved
+
+var path: Array
+var is_moving: bool = true
 
 var _targets: Array = []
 var _magasine: int = 0
 
+var pathing_manager: PathingManager
+var navigation_agent: NavigationAgent2D
+
+@export_category("Properties")
+@export var movement_speed: int = 200
+@export var health: int = 5
+@export var chance_to_move: float = 0.1
+
+@export_category("Weapon")
 @export var time_between_shots: float = 1.5
 @export var time_reload: float = 3
 @export var magasine_size: int = 6
@@ -21,13 +34,23 @@ var bomb_scene: PackedScene = preload("res://Projectiles/Bomb/bomb.tscn")
 
 @onready var particles: GPUParticles2D = $GPUParticles2D
 
-@export var health: int = 5
-
 func _ready() -> void:
+	pathing_manager = $PathingManager
+	pathing_manager.path = path
+	navigation_agent = $NavigationAgent2D
+	navigation_agent.connect("target_reached", _on_reached_target)
 	_magasine = magasine_size
-	timer.connect("timeout", _on_timer_timeout)
-	timer.start()
+	timer.connect("timeout", _on_shoot_timer)
+	navigation_agent.target_position = pathing_manager.get_target()
 
+func _physics_process(_delta: float) -> void:
+	if is_moving:
+		move()
+
+func move():
+	var path_position = navigation_agent.get_next_path_position()
+	velocity = global_position.direction_to(path_position) * movement_speed
+	move_and_slide()
 
 # TODO re-consider spawn position (start_pos) for the bullet
 func _shoot(target_pos: Vector2) -> void:
@@ -64,8 +87,18 @@ func hit():
 	if health < 1:
 		emit_signal("death", self)
 
+func _begin_movement():
+	navigation_agent.target_position = pathing_manager.get_target()
+	is_moving = true
+
 #Signals
-func _on_timer_timeout() -> void:
+func _on_shoot_timer() -> void:
+	if is_moving: return
+	if pathing_manager:
+		if randf() < chance_to_move:
+			_begin_movement()
+			return
+	
 	if len(_targets) > 0:
 		_shoot(_targets.pick_random())
 	
@@ -74,3 +107,8 @@ func _on_timer_timeout() -> void:
 	else:
 		_magasine = magasine_size
 		timer.start(time_reload)
+
+func _on_reached_target():
+	is_moving = false
+	timer.start(time_between_shots)
+	emit_signal("moved")
