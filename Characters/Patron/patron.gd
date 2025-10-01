457b4c96a4e053fcbf05ca1b5f1704c1d73db3b6
SnowyPeax <johbr@itu.dk>
2025-10-01 20:46:39 +0200

    update enemy movement

--- Characters/Patron/patron.gd
+++ Characters/Patron/patron.gd
@@ -18,7 +18,9 @@ var sprite_bandit: Texture2D = preload("res://Assets/saloon_NPC_bandit.png")
 @export_category("Properties")
 @export var movement_speed: int = 200
 @export var health: int = 5
-@export var chance_to_move: float = 0.1
+@export var can_shoot_while_moving: bool = true
+@export var time_between_move_min: float = 10.0
+@export var time_between_move_max: float = 10.0
 
 @export_category("Weapon")
 @export var time_between_shots: float = 1.5
@@ -28,7 +30,8 @@ var sprite_bandit: Texture2D = preload("res://Assets/saloon_NPC_bandit.png")
 @export var bullet_speed: int = 500
 @export var spray_angle_degrees: float = 20
 
-@onready var timer: Timer = $Timer
+@onready var timer: Timer = $ShootingTimer
+@onready var moving_timer: Timer = $MovingTimer
 var bullet_scene: PackedScene = preload("res://Projectiles/Bullet/bullet.tscn")
 
 @export var bomb_chance: float = 0.1
@@ -46,6 +49,8 @@ func _ready() -> void:
 	_magasine = magasine_size
 	timer.connect("timeout", _on_shoot_timer)
 	navigation_agent.target_position = pathing_manager.get_target()
+	moving_timer.connect("timeout", _on_moving_timer)
+	_start_move_timer()
 
 func _physics_process(_delta: float) -> void:
 	if is_moving:
@@ -94,14 +99,17 @@ func hit():
 func _begin_movement():
 	navigation_agent.target_position = pathing_manager.get_target()
 	is_moving = true
+	
+	_start_move_timer()
+
+func _start_move_timer() -> void:
+	var time_to_next_move = randf_range(time_between_move_min, time_between_move_max)
+	moving_timer.start(time_to_next_move)
 
 #Signals
 func _on_shoot_timer() -> void:
-	if is_moving: return
-	if pathing_manager:
-		if randf() < chance_to_move:
-			_begin_movement()
-			return
+	if not can_shoot_while_moving and is_moving:
+		return
 	
 	if len(_targets) > 0:
 		_shoot(_targets.pick_random())
@@ -116,3 +124,9 @@ func _on_reached_target():
 	is_moving = false
 	timer.start(time_between_shots)
 	emit_signal("moved")
+
+func _on_moving_timer() -> void:
+	if not is_moving and pathing_manager:
+		_begin_movement()
+	else:
+		_start_move_timer()
